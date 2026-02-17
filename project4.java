cc0527b99e1304b3a29943e7aec36727d6e20760
as7013-boop <as7013@srmist.edu.in>
2026-02-17 10:30:06 +0530

    Add files via upload

---
+++ project4.java
@@ -0,0 +1,22 @@
+public class project4 {
+    
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            " *****   *****   *****   ***** ",
+            "*     * *     * *     * *      ",
+            "*     * *     * *     * *      ",
+            "*     * *     * *****    ***** ",
+            "*     * *     * *            * ",
+            "*     * *     * *            * ",
+            " *****   *****  *        ***** ",
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
+    
+
